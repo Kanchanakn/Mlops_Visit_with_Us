14a09988b27a1e9a3fc00e37363b6142e871852c
Kanchanakn <45705059+Kanchanakn@users.noreply.github.com>
2026-03-01 11:01:14 +0530

    Update app.py

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -62,4 +62,5 @@ if st.button("Predict Wellness Package Purchase"):
     prediction = model.predict(input_data)[0]
     result = "Purchase" if prediction == 1 else "No Purchase"
     st.subheader("Prediction Result:")
-    st.success(f"The model predicts: **{result}**: ", prediction)
+    st.write(f"The predicted status for the customer is {prediction[0]}")
+    st.success(f"The model predicts: **{result}**")
